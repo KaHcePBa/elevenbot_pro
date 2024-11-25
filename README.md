78a6bc63b1dca6f5c6a7c91799df2a2438190dc9
KaHcePBa <godhead@tut.by>
2024-11-25 21:31:46 +0300

    switch to aiogram 3.x

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [![Python](https://img.shields.io/badge/Python-3.x-blue.svg)](https://www.python.org/) 
-[![Aiogram](https://img.shields.io/badge/Aiogram-v2.25.1-orange.svg)](https://docs.aiogram.dev)
-[![Dynaconf](https://img.shields.io/badge/Dynaconf-3.1.11-blue.svg)](https://dynaconf.readthedocs.io/en/latest/)
+[![Aiogram](https://img.shields.io/badge/Aiogram-v3.15-orange.svg)](https://docs.aiogram.dev)
+[![Dynaconf](https://img.shields.io/badge/Dynaconf-3.2.6-blue.svg)](https://dynaconf.readthedocs.io/en/latest/)
 [![Aioschedule](https://img.shields.io/badge/Aioschedule-0.5.2-orange.svg)](https://aioschedule.readthedocs.io/en/latest/)
 [![Requests](https://img.shields.io/badge/Requests-2.x-brightgreen.svg)](https://docs.python-requests.org/en/latest/)
 
---
+++ features/description.py
@@ -0,0 +1,19 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+
+# Создаем отдельный Router для описания
+description_router = Router()
+
+
+@description_router.message(Command("description"))
+async def description_message(message: Message):
+    desc_message = """
+<b>Voice-recognition technology?</b> In a lift? In Scotland? You ever tried voice-recognition technology?\n
+👇 Delivered functions:
+1️⃣ enter the command <b>''' /weather city '''</b> to get weather for now
+2️⃣ checking birthdays of everyone and pin congrats with notification
+3️⃣ enter the command <b>''' /gpt your question '''</b> to get info from ChatGPT (update: token is expired, keep calm)
+    """
+    await message.answer(desc_message, parse_mode="HTML")
+    await message.delete()
