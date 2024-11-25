f0e0384332014c555b2df2187e37c33824cd85c2
KaHcePBa <godhead@tut.by>
2024-11-25 22:24:24 +0300

    description update

--- routers/description.py
+++ routers/description.py
@@ -5,15 +5,16 @@ from aiogram.types import Message
 # Создаем отдельный Router для описания
 description_router = Router()
 
+desc_message = """
+<b>Voice-recognition technology?</b> In a lift? In Scotland?\nYou ever tried voice-recognition technology?\n
+👇 Delivered functions: 👇
+1️⃣✅ <b>''' /weather city '''</b> to get weather for now
+2️⃣❌ <i> [depreciated] </i> check birthdays and pin congrats with notify
+3️⃣❌ <i> [depreciated] </i> <b>''' /gpt your question '''</b> ChatGPT 
+"""
+
 
 @description_router.message(Command("description"))
 async def description_message(message: Message):
-    desc_message = """
-<b>Voice-recognition technology?</b> In a lift? In Scotland? You ever tried voice-recognition technology?\n
-👇 Delivered functions:
-1️⃣✅ enter the command <b>''' /weather city '''</b> to get weather for now
-2️⃣❌ checking birthdays of everyone and pin congrats with notification
-3️⃣❌ enter the command <b>''' /gpt your question '''</b> to get info from ChatGPT
-    """
     await message.answer(desc_message, parse_mode="HTML")
     await message.delete()
