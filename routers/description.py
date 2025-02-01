544227b8d227058f81856d4e1e76c855d698de0a
KaHcePBa <godhead@tut.by>
2025-02-01 23:25:04 +0300

    desc update

--- routers/description.py
+++ routers/description.py
@@ -8,9 +8,9 @@ description_router = Router()
 desc_message = """
 <b>Voice-recognition technology?</b> In a lift? In Scotland?\nYou ever tried voice-recognition technology? 🤔\n
 👇 Delivered functions: 👇
-1️⃣✅ <b><code>/weather</code></b> city to get weather 🌤️ for now
-2️⃣❌ <i> [depreciated] </i> Check birthdays 🎉 and pin congratulations with notifications.
-3️⃣❌ <i> [need billing] </i> <b>''' /gpt your question '''</b> Ask ChatGPT a question. 🤖
+1️⃣✅ <b><code>/weather city</code></b> to get weather 🌤️ for now
+2️⃣❌ <i>[depreciated]</i> сheck birthdays 🎉 and pin congratulations with notifications.
+3️⃣❌ <i>[need billing]</i> <b><code>/gpt your_question</code></b> ask ChatGPT a question. 🤖
 ✨ <i>More features coming soon!</i> ✨
 """
 
