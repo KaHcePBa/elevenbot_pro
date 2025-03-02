f81c9a7f7deec042978802de6d79236c33a262f3
KaHcePBa <godhead@tut.by>
2025-03-02 23:03:21 +0300

    ParseMode

--- handlers/ai.py
+++ handlers/ai.py
@@ -1,5 +1,6 @@
 from aiogram import F, Router
 from aiogram.types import Message
+from aiogram.enums import ParseMode
 
 from app import get_ai_response
 
@@ -17,4 +18,4 @@ async def handle_gpt_command(message: Message):
 
     await message.answer("Hold on, your request is being generated....")
     ai_response = await get_ai_response(user_question)
-    await message.answer(ai_response)
+    await message.answer(ai_response, parse_mode=ParseMode.MARKDOWN_V2)
