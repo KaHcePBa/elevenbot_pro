0355dfb5e48fd85d77ae47cbc6e3ef7a4122690d
KaHcePBa <godhead@tut.by>
2025-03-02 22:37:17 +0300

    Revert "ai class Gen"
    This reverts commit 621c35c3cc2c2e49abab1935a14e45cc4f30614b.

--- handlers/ai.py
+++ handlers/ai.py
@@ -1,7 +1,4 @@
 from aiogram import F, Router
-from aiogram.enums import ParseMode
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import Message
 
 from app import get_ai_response
@@ -10,11 +7,7 @@ from app import get_ai_response
 ai_router = Router()
 
 
-class Gen(StatesGroup):
-    wait = State()
-
-
-@ai_router.message(F.text.startswith('/ai'), state=FSMContext)
+@ai_router.message(F.text.startswith('/ai'))
 async def handle_gpt_command(message: Message):
     # Extracting the question from the command
     user_question = message.text.lstrip('/ai').strip()
@@ -22,11 +15,6 @@ async def handle_gpt_command(message: Message):
         await message.answer("Please write a question after the command /ai.")
         return
 
-    await message.answer(Gen.wait)
+    await message.answer("Thinking about the answer... Keep calm...")
     ai_response = await get_ai_response(user_question)
-    await message.answer(ai_response, parse_mode=ParseMode.MARKDOWN)
-
-
-@ai_router.message(Gen.wait)
-async def stop_flood(message: Message):
-    await message.answer("Hold on, your request is being generated....")
+    await message.answer(ai_response)
