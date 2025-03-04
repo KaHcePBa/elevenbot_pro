9cbf4b797caaf67235b33688d3c96e77a8b493a1
KaHcePBa <godhead@tut.by>
2025-03-05 00:49:11 +0300

    pravo update

--- handlers/pravo.py
+++ handlers/pravo.py
@@ -10,9 +10,9 @@ pravo_router = Router()
 async def pravonn_message(message: Message):
     user_id_firstname = message.from_user.first_name
     pnn_message = f"""
-👑👑👑 <b>Внимание!</b> 👑👑👑\n
-Для <b>{user_id_firstname}</b>
-предоставлено правонанахуй!
+👑👑👑 <b>Attention!</b> 👑👑👑\n
+For <b>{user_id_firstname}</b>.
+granted by the right to fuck off!
     """
     await message.answer(pnn_message, parse_mode="HTML")
     await message.delete()
