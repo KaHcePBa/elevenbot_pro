983b8a5b4cb7e0a2a8ba9c616fcabbdd32fbdcc6
KaHcePBa <godhead@tut.by>
2025-09-24 00:31:52 +0300

    rollback

--- config/config.py
+++ config/config.py
@@ -7,5 +7,5 @@ settings = LazySettings(
     env='development',
 )
 
-# `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
+#  `envvar_prefix` = export envvars with `export DYNACONF_FOO=bar`.
 # `settings_files` = Load these files in the order.
